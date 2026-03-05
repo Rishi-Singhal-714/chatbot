c04117cb1f9e385f477106021d0b701054a8ee40
ZULU-CLUB <zuluclubindia@gmail.com>
2026-03-05 20:03:25 +0530

    updates

--- requestData.js
+++ requestData.js
@@ -105,7 +105,7 @@ const columnMapping = {
   'visit_us', 'home_banner', 'watch_banner', 'visit_banner', 'listen_banner',
   'cart_banner', 'profile_bg', 'concierge', 'created_at', 'updated_at','visit2_bg',
   'watch2_bg','decorate2_bg','ask_zulu2_bg','sound2_bg','concierge2_bg','shop_bg',
-  'watch_bg','visit_bg','chat_bg','decorate_bg','feel_bg'
+  'watch_bg','visit_bg','chat_bg','decorate_bg','feel_bg','primary_color','secondary_color'
 ],  
 'moods': [
     'id', 'user_mood', 'parent_mood', 'description',
